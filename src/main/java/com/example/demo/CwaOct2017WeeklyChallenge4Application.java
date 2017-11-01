2669d6bc9a6b3704b62dd2e20d664a421ae25544
Joyce Yang <jcyang@hmc.edu>
2017-11-01 17:02:07 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/CwaOct2017WeeklyChallenge4Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwaOct2017WeeklyChallenge4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwaOct2017WeeklyChallenge4Application.class, args);
+	}
+}
